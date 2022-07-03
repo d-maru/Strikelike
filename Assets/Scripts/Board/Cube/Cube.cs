5baefcf9d76078c8ef803db3174334beb8239296
Ma <255f7fc264557@gmail.com>
2022-07-03 21:16:46 +0900

    移動可能範囲判定を追加_amend

---
+++ Assets/Scripts/Board/Cube/Cube.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// CubeBaseを継承したクラス(未実装)
+/// </summary>
+public class Cube : CubeBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
